d42b185d03e1ac2cc4c987510aac25d05fa9a20e
Messi <994643639@qq.com>
2019-01-05 15:05:41 +0800

    mxww

--- src/main/java/com/mxw/springboot/SpringbootApplication.java
+++ src/main/java/com/mxw/springboot/SpringbootApplication.java
@@ -11,6 +11,7 @@ public class SpringbootApplication {
        ConfigurableApplicationContext context=SpringApplication.run(SpringbootApplication.class, args);
        System.out.println(context.getEnvironment().getProperty("jdbc.password"));
        System.out.println(context.getEnvironment().getProperty("jdbc.name"));
+        System.out.println(context.getEnvironment().getProperty("111"));
        context.close();
     }
 
